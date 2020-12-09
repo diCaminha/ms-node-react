ed0bc3858466b15da038c8457930c1fb06dbdd91
Denis <dineresc@gmail.com>
2020-12-09 13:51:03 -0300

    finish ms comment

--- microservices/comments/index.js
+++ microservices/comments/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +10,30 @@ app.use(cors());
 
 const commentsByPost = {};
 
-app.get("/posts/:id/comments", (req,res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
+
+app.post("/posts/:id/comments", (req, res) => {
+    const postId = req.params.id;
+    const comments = commentsByPost[postId].comments || [];
+    
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+    const comment = {commentId,content};
+
+    comments.push(comment);
+
+    commentsByPost[postId] = comments;
+
+    await axios.post(
+        "http://localhost:4005/events",
+        {
+            type: "CommentCreated",
+            data: {id: commentId, content, postId}
+        });
+
+    res.status(201).send(comments);
+});
+
+
